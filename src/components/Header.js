877e3c078fe34860f2ea7fb56e32dbdb85240fb3
Doan Hong Ngoc <ngocdh236@gmail.com>
2019-09-25 13:09:46 +0300

    Setup project, add header

---
+++ src/components/Header.js
@@ -0,0 +1,56 @@
+import React, { useReducer, useEffect } from 'react';
+
+import '../styles/Header.scss';
+
+export default function Header() {
+  const jobTitleReducer = (state, action) => {
+    const { type, character } = action;
+    switch (type) {
+      case 'ADD_CHARACTER':
+        return state + character;
+      case 'REMOVE_CHARACTER':
+        return state.slice(0, -1);
+      default:
+        return state;
+    }
+  };
+
+  const [jobTitle, dispatchJobTitle] = useReducer(jobTitleReducer, '');
+
+  useEffect(() => {
+    var timer = 100;
+    const interval = setInterval(() => {
+      const jobTitleCharacters = 'Junior Software Developer'.split('');
+
+      jobTitleCharacters.forEach(character => {
+        setTimeout(() => {
+          dispatchJobTitle({ type: 'ADD_CHARACTER', character });
+        }, timer);
+        timer += 80;
+      });
+
+      timer += 800;
+
+      jobTitleCharacters.forEach(character => {
+        setTimeout(() => {
+          dispatchJobTitle({ type: 'REMOVE_CHARACTER', character });
+        }, timer);
+        timer += 40;
+      });
+
+      timer += 800;
+    }, timer);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <header className='d-flex justify-content-center align-items-center text-center'>
+      <div className='text-background'></div>
+      <div className='text'>
+        <h1 className='mb-4'>Ngoc Doan</h1>
+        <p className='lead'>{`${jobTitle}|`}</p>
+      </div>
+    </header>
+  );
+}
